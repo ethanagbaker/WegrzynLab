7371aeec685146aeea642ee3775dbf834cdf9389
Ethan Baker <user@d89h142.public.uconn.edu>
2014-07-11 15:10:27 -0400

    added commenting

--- remove_protein_contaminants.py
+++ remove_protein_contaminants.py
@@ -4,10 +4,13 @@
 ##Author: Ethan Baker
 ## Computational Genomics Lab, University of Connecticut
 ##
-##Last Update 7/10/14
+##Last Update 7/11/14
+
+print "Getting started..."
  
 from Bio import SeqIO
-print "Getting started..."
+
+#Part 0 - Get file paths
 originalprotseq = raw_input("Enter the path of the contaminated protein FASTA file (relative is OK!): ")
 contaminantsfile = raw_input("Enter the path of the contaminants job.py out file (relative is OK!): ")
 
@@ -17,14 +20,14 @@ with open(originalprotseq,'r') as pepfile:
 		print "Loading original FASTA protein file...."
 		for line in pepfile:
 			if line.startswith('>'):
-				x = line.split('|', 1)[0]
+				x = line.split('|', 1)[0]      #Splits the header line to minimal amount to be unique
 				#print x
-				outputfile.write(x)
+				outputfile.write(x)				#Writes header line. 
 				outputfile.write('\n')
 			else:
 				#print line
-				outputfile.write(line)
-		print "FASTA modification complete."
+				outputfile.write(line)			#Writes sequence
+		print "FASTA modification complete!"
 outputfile.close()
 
 #Part 2 - Make list of contaminant ID numbers. 
@@ -33,8 +36,8 @@ with open(contaminantsfile,'r') as contaminants_table:
 	print "Loading contaminants table..."
 	for line in contaminants_table:
 		firstcol = line.split('\t',1)[0]
-		idnum = firstcol.split('|', 1)[0]
-		nameslist.write('cds.')
+		idnum = firstcol.split('|', 1)[0] 		#Gets minimal unique portion (same as in part 1)
+		nameslist.write('cds.')					#Required to match format of FASTA headers. 
 		nameslist.write(idnum)
 		nameslist.write('\n')
 print "Contaminant names list generated!"
@@ -49,10 +52,10 @@ with open('proteincontaminants.list', 'r') as namefile:
 uncontaminated=[]
 print 'Removing contaminants...'
 for record in SeqIO.parse('centroids.fasta.transdecoder.pep.mod', "fasta"):
-	if record.id not in headerlines:
+	if record.id not in headerlines:		#If file is not in the contaminants list, it must be clean, so append it to the uncontaminated list. 
 		uncontaminated.append(record)
 print "Generating clean sequence file..."
 finaloutput=open("uncontaminatedseqs.pep","w")
-SeqIO.write(uncontaminated, finaloutput, "fasta")
+SeqIO.write(uncontaminated, finaloutput, "fasta")	#Writes FASTA file
 finaloutput.close()	
 print "Done!"		
